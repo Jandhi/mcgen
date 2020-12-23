2cef0725658c720769caa8dcfc956e7f1f9007fe
Jandhi <jpdohrin@uwaterloo.ca>
2020-12-22 20:33:02 -0500

    Filling boxes

--- filter.py
+++ filter.py
@@ -1,5 +1,8 @@
 from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
 from mcplatform import *
+from utility import fill
 
 def perform(level, box, options):
-    pass
+    point1 = (box.minx, box.miny, box.minz)
+    point2 = (box.maxx, box.maxy, box.maxz)
+    fill(point1, point2, level)
