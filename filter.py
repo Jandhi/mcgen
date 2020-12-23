2e67637fd822051c470b65dbea0315396d99d135
zachmanson <68168088+zachmanson@users.noreply.github.com>
2020-12-22 22:20:32 -0500

    added map, yoinked mapimage

--- filter.py
+++ filter.py
@@ -2,11 +2,13 @@ from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
 from mcplatform import *
 from utility import fill
 from material import WeightedMaterial
+from map import Map
 
 def perform(level, box, options):
     point1 = (box.minx, box.miny, box.minz)
     point2 = (box.maxx, box.maxy, box.maxz)
 
     material = WeightedMaterial( [((4, 0), 3) , ((48, 0), 1)] )
+    map = Map(box, level)
 
     fill(point1, point2, level, material)
